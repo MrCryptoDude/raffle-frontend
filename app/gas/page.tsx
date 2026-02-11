71d23cf4c180a774ad99eaab1b384dd2ed922861
Dr.Crypto <george.rmaili@gmail.com>
2026-02-11 23:47:12 +0100

    a

--- app/gas/page.tsx
+++ app/gas/page.tsx
@@ -83,7 +83,7 @@ const marketAbi = [
 const erc20Abi = [
   { type: "function", name: "allowance", stateMutability: "view", inputs: [{ name: "owner", type: "address" }, { name: "spender", type: "address" }], outputs: [{ type: "uint256" }] },
   { type: "function", name: "approve", stateMutability: "nonpayable", inputs: [{ name: "spender", type: "address" }, { name: "amount", type: "uint256" }], outputs: [{ type: "bool" }] },
-  { type: "function", name: "balanceOf", stateMutability: "nonpayable", inputs: [{ name: "account", type: "address" }], outputs: [{ type: "uint256" }] },
+  { type: "function", name: "balanceOf", stateMutability: "view", inputs: [{ name: "account", type: "address" }], outputs: [{ type: "uint256" }] },
 ] as const;
 
 // ----------------------------
